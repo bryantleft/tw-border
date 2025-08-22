13d8fb38164f7f003e9f7703c215de0d60de81fe
Bryant Le <bryantle2021@gmail.com>
2025-08-22 16:22:48 -0500

    feat: dynamic dash count

--- example/app/page.tsx
+++ example/app/page.tsx
@@ -4,20 +4,17 @@ export default function Home() {
       <div className="p-8">
         <div className="grid grid-cols-1 gap-8 items-center justify-items-center">
           <div>
-            <h3 className="text-sm font-semibold mb-4 text-center">Small</h3>
-            <div className="border-2 border-dashed border-animate bg-gray-300 w-[150px] h-[100px] rounded-md">
+            <div className="border-2/30 border-dashed border-animate bg-gray-300 w-[150px] h-[100px]">
               <svg className="border-svg"><rect /></svg>
             </div>
           </div>
           <div>
-            <h3 className="text-sm font-semibold mb-4 text-center">Medium</h3>
-            <div className="border border-dashed border-animate bg-gray-300 w-[300px] h-[200px] rounded-md">
+            <div className="border-[4px] border-dashed border-animate bg-gray-300 w-[300px] h-[200px] rounded-xl">
               <svg className="border-svg"><rect /></svg>
             </div>
           </div>
           <div>
-            <h3 className="text-sm font-semibold mb-4 text-center">Large</h3>
-            <div className="border-3 border-dashed border-animate bg-gray-300 w-[450px] h-[300px] rounded-full">
+            <div className="border-3 border-[#ff0000]/30 border-dashed hover:border-animate bg-red-100 w-[450px] h-[300px] rounded-full">
               <svg className="border-svg"><rect /></svg>
             </div>
           </div>
